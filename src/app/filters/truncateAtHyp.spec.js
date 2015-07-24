cc7b185d9e54200f8619f5af40fd6b6a4fc9d10c
unknown <susansouza@DEV-MFILIPSKI2.percussion.local>
2015-07-24 15:28:43 -0400

    Whoops missed a semicolon.

--- src/app/filters/truncateAtHyp.spec.js
+++ src/app/filters/truncateAtHyp.spec.js
@@ -15,7 +15,7 @@
 
     it('should return the input truncated at the first hyphen encountered, replacing the following text with an ellipsis', function(){
       var text = "some nonsense string here- more following nonsense";
-      expect(truncateAtHyp(text)).toBe("some nonsense string here...")
+      expect(truncateAtHyp(text)).toBe("some nonsense string here...");
     });
 
   });
