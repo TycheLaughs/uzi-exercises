b343efea15ac9660d744bb23d3d5e7f249f1da50
unknown <susansouza@DEV-MFILIPSKI2.percussion.local>
2015-07-24 15:29:15 -0400

    And another.

--- src/app/filters/trustHtml.spec.js
+++ src/app/filters/trustHtml.spec.js
@@ -15,7 +15,7 @@
 
     it('should return the input as html, with several invalid substrings replaced', function () {
       var text = "<b>http%3A%2F%2Fwww.cnn.com%2F2015%2F07%2F22%2Fpolitics%2Fobama-family-kenya-brooke-baldwin%2F</b>&nbsp;";
-      expect(trustHtml(text).$$unwrapTrustedValue()).toBe("http%3A%2F%2Fwww.cnn.com%2F2015%2F07%2F22%2Fpolitics%2Fobama-family-kenya-brooke-baldwin%2F")
+      expect(trustHtml(text).$$unwrapTrustedValue()).toBe("http%3A%2F%2Fwww.cnn.com%2F2015%2F07%2F22%2Fpolitics%2Fobama-family-kenya-brooke-baldwin%2F");
     });
 
   });
