ffa33834d31acf4dc782e91d51bfd0cf0eb2b2ca
unknown <susansouza@DEV-MFILIPSKI2.percussion.local>
2015-07-29 13:11:59 -0400

    Started blocking out GUI.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,101 +6,112 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+  function MainController($scope) {
+    $scope.users =  [{
+      "First":"Sponge",
+      "Last":"Bob",
+      "Sex":"Male",
+      "Age":"24",
+      "Title":"Frycook",
+      "ID":"0"},
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1438186125603;
-    vm.showToastr = showToastr;
+      {
+        "First":"Oliver",
+        "Last":"Queen",
+        "Sex":"Male",
+        "Age":"30",
+        "Title":"Superhero",
+        "ID":"1"}
+    ];
 
     activate();
 
     function activate() {
 
 
-      var users = [{
-        "First":"Sponge",
-        "Last":"Bob",
-        "Sex":"Male",
-        "Age":"24",
-        "Title":"Frycook",
-        "ID":"0"},
-
-        {
-          "First":"Oliver",
-          "Last":"Queen",
+        var users = [{
+          "First":"Sponge",
+          "Last":"Bob",
           "Sex":"Male",
-          "Age":"30",
-          "Title":"Superhero",
-          "ID":"1"}
-      ];
+          "Age":"24",
+          "Title":"Frycook",
+          "ID":"0"},
 
-      var i = 1;
-      function createUsers(first, last, sex, age, title){
-        i++;
-        var obj = {
-          "First":"",
-          "Last":"",
-          "Sex":"",
-          "Age":"",
-          "Title":"",
-          "ID": i
-        };
-        obj.First = first;
-        obj.Last = last;
-        obj.Sex = sex;
-        obj.Age = age;
-        obj.Title = title;
-        users.push(obj);
-      }
+          {
+            "First":"Oliver",
+            "Last":"Queen",
+            "Sex":"Male",
+            "Age":"30",
+            "Title":"Superhero",
+            "ID":"1"}
+        ];
 
-
-      function updateUser(id, key, value){
-        var j;
-
-
-
-
-
-        for(j = 0; j < users.length; j++){
+        var i = 1;
+        function createUsers(first, last, sex, age, title){
+          i++;
+          var obj = {
+            "First":"",
+            "Last":"",
+            "Sex":"",
+            "Age":"",
+            "Title":"",
+            "ID": i
+          };
+          obj.First = first;
+          obj.Last = last;
+          obj.Sex = sex;
+          obj.Age = age;
+          obj.Title = title;
+          users.push(obj);
+        }
 
 
-
-          console.log('=============');
-          console.log(users[j]);
-          console.log('=============');
+        function updateUser(id, key, value){
+          var j;
 
 
-          if(users[j].ID === id){
-            if(users[j].hasOwnProperty(key)){
-              Object.defineProperty(users[j], key, {value: value});
+
+
+
+          for(j = 0; j < users.length; j++){
+
+
+
+            console.log('=============');
+            console.log(users[j]);
+            console.log('=============');
+
+
+            if(users[j].ID === id){
+              if(users[j].hasOwnProperty(key)){
+                Object.defineProperty(users[j], key, {value: value});
+              }
             }
           }
         }
-      }
-      function removeUser(id){
-        var j;
-        for(j = users.length-1; j >0; j--){
-          if(users[j].ID === id){
-            users.splice(j, 1);
+        function removeUser(id){
+          var j;
+          for(j = users.length-1; j >0; j--){
+            if(users[j].ID === id){
+              users.splice(j, 1);
+            }
           }
         }
-      }
-      function readUsers(users){
+        function readUsers(users){
 
-        var j;
-        for(j = 0; j < users.length; j++){
-          console.log(users[j].First + ' ' + users[j].Last);
+          var j;
+          for(j = 0; j < users.length; j++){
+            console.log(users[j].First + ' ' + users[j].Last);
+          }
+
         }
 
-      }
-
-      createUsers("Doug");
-      updateUser(2, "Last", "samper");
-      updateUser(2, "Age", "30");
-      readUsers(users);
-      removeUser(2);
-      readUsers(users);
+        createUsers("Doug");
+        updateUser(2, "Last", "samper");
+        updateUser(2, "Age", "30");
+        readUsers(users);
+        removeUser(2);
+        readUsers(users);
+    }
   }
-})();
+}());
