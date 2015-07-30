954edc4b5b72b4a885012c2297e7a8f495a3a6d3
unknown <susansouza@DEV-MFILIPSKI2.percussion.local>
2015-07-30 12:11:28 -0400

    Started service tests.

--- src/app/components/lastChatter/lastChatter.service.spec.js
+++ src/app/components/lastChatter/lastChatter.service.spec.js
@@ -1,3 +1,42 @@
 /**
  * Created by susansouza on 7/30/2015.
  */
+(function() {
+  'use strict';
+
+  describe('Service: lastChatter', function(){
+
+    beforeEach(module('musicCatalog'));
+    var  lastChatter, httpBackend, http;
+    beforeEach(inject(function (_lastChatter_, $httpBackend, $http) {
+
+      lastChatter = _lastChatter_;
+      http = $http;
+      httpBackend = $httpBackend;
+      httpBackend.when("GET", "").respond(
+        {});
+  }));
+
+    /*
+
+    Functions
+
+
+     */
+
+    describe('Function: getTopTracks(artist)', function(){
+      it('should have a function to get songs by artist from last.fm that returns a promise', function(){
+
+
+      });
+    });
+
+    describe('Function: getbuyLinks(artist, song, country)', function(){
+      it('should have a function to get links to where to buy the selected track  from last.fm that returns a promise', function(){
+
+
+      });
+    });
+});
+
+}());
