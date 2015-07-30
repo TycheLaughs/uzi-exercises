edf5706a970d624556fdf0247d0fc972fcb642e0
unknown <susansouza@DEV-MFILIPSKI2.percussion.local>
2015-07-30 14:25:08 -0400

    Revised lastChatter service tests.

--- src/app/components/lastChatter/lastChatter.service.spec.js
+++ src/app/components/lastChatter/lastChatter.service.spec.js
@@ -17,6 +17,11 @@
 
 
         }));
+      afterEach(function(){
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+
+      });
 
         /*
 
@@ -26,26 +31,33 @@
          */
 
         describe('Function: getTopTracks(artist)', function () {
-            it('should have a function to get songs by artist from last.fm that returns a promise', function () {
-              httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&format=json&method=artist.getTopTracks").respond({
-                "result": [{
-                  "artist": "Cher"
-                }]
-              });
-              httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=&autocorrect=1&format=json&method=artist.getTopTracks").respond({
-                "result": "error"
-              });
-                var res;
-                httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&format=json&method=artist.getTopTracks");
-                lastChatter.getTopTracks("Cher").then(function (resp) {
-                    res = resp.result;
-                }, function (err) {
-                    res = err.result;
-                });
-                httpBackend.flush();
-                expect(res.length).toBe(1);
+          beforeEach(function(){
+            httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&format=json&method=artist.getTopTracks").respond({
+              "result": [{
+                "artist": "Cher"
+              }]
+            });
+            httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=&autocorrect=1&format=json&method=artist.getTopTracks").respond({
+              "result": "error"
+            });
+          });
 
-                httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=&autocorrect=1&format=json&method=artist.getTopTracks");
+          it('should get songs by artist from last.fm that returns a promise', function () {
+
+            var res;
+            httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&format=json&method=artist.getTopTracks");
+            lastChatter.getTopTracks("Cher").then(function (resp) {
+              res = resp.result;
+            }, function (err) {
+              res = err.result;
+            });
+            httpBackend.flush();
+            expect(res.length).toBe(1);
+          });
+
+          it('should trigger an error callback function when incorrect arguments are passed', function(){
+            var res;
+            httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=&autocorrect=1&format=json&method=artist.getTopTracks");
                 lastChatter.getTopTracks("").then(function (resp) {
                     res = resp.result;
                 }, function (err) {
@@ -58,15 +70,17 @@
         });
 
         describe('Function: getbuyLinks(artist, song, country)', function () {
-            it('should have a function to get links to where to buy the selected track  from last.fm that returns a promise', function () {
-              httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=Believe").respond(
-                {"result":[{"artist":"Cher"}]
-                });
-              httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=").respond(
-                {"result":"error"
-                });
-              var res;
+          beforeEach(function(){
+            httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=Believe").respond(
+              {"result":[{"artist":"Cher"}]
+              });
+            httpBackend.when("GET", "http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=").respond(
+              {"result":"error"
+              });
+          });
+            it('should get links to where to buy the selected track  from last.fm that returns a promise', function () {
 
+              var res;
 
               httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=Believe");
               lastChatter.getBuyLinks("Cher", "Believe", "US").then(function (resp) {
@@ -76,8 +90,10 @@
               });
               httpBackend.flush();
               expect(res.length).toBe(1);
-
-              httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=");
+            });
+          it('should trigger an error callback function when incorrect arguments are passed', function(){
+            var res;
+            httpBackend.expectGET("http://ws.audioscrobbler.com/2.0?api_key=f3e6bff38901e60e008579851e02440a&artist=Cher&autocorrect=1&country=US&format=json&method=track.getBuyLinks&track=");
               lastChatter.getBuyLinks("Cher", "", "US").then(function (resp) {
                 res = resp.result;
               }, function (err) {
